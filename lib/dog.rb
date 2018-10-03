bb34d180d49ff937b6ec300801efaa6b28dfc9da
Harry Bloch <harryfbloch@gmail.com>
2018-10-03 20:38:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    self.name = name
+    self.mood = "nervous"
+  end
+  
+  private
+  def name=(name)
+    @name = name
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name)
+    self.name = name
+    self.mood = "nervous"
+  end
+  
+  private
+  def name=(name)
+    @name = name
+  end
 end
